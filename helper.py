4928349b9c416609abedf14c9f0ed1515893bf53
Kathryn <kpanger88@gmail.com>
2021-10-22 04:56:58 -0700

    Fixed transform error

--- helper.py
+++ helper.py
@@ -7,5 +7,5 @@ while working:
     os.system("git add .")
     os.system(f"git commit -m \"dealing with time-shifting pt{counter}\"")
     os.system("git push origin master")
-    time.sleep(3600)
+    time.sleep(3601)
     continue
