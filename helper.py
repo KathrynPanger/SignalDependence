90555a519f5ffbb72d807cf3106822412674b035
Kathryn <kpanger88@gmail.com>
2021-10-21 21:13:51 -0700

    auto commit to maintain focus

---
+++ helper.py
@@ -0,0 +1,9 @@
+import os
+turnCount=0
+working=1
+while working:
+    os.system("git add .")
+    os.system("git commit -m \"auto commit to maintain focus\" ")
+    os.system("git push origin master")
+    time.sleep(3600)
+    continue
