939dfe71e8b6091fa8841112e60bab9b8443f446
Kathryn <kpanger88@gmail.com>
2021-10-23 04:44:59 -0700

    machine learning pt 1

--- helper.py
+++ helper.py
@@ -5,7 +5,7 @@ working=1
 counter=1
 while working:
     os.system("git add .")
-    os.system(f"git commit -m \"dealing with time-shifting pt{counter}\"")
+    os.system(f"git commit -m \"machine learning pt {counter}\"")
     os.system("git push origin master")
     time.sleep(2000)
     continue
